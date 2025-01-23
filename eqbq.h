7c4b5038612c57ecd30e47c7b406333de8e11160
rafal <redaf@users.noreply.github.com>
2025-01-23 01:27:31 +0100

    align

--- eqbq.h
+++ eqbq.h
@@ -40,24 +40,28 @@ EQBQ_CDEF void eqbq_low_pass(double coeffs[6], double norm_freq, double q)
 {
     const double w0 = 2.0 * EQBQ_PI * norm_freq;
     const double alpha = sin(w0) / (2 * q);
+    // clang-format off
     coeffs[0] = (1.0 - cos(w0)) / 2.0;
-    coeffs[1] = 1.0 - cos(w0);
+    coeffs[1] =  1.0 - cos(w0);
     coeffs[2] = (1.0 - cos(w0)) / 2.0;
-    coeffs[3] = 1.0 + alpha;
+    coeffs[3] =  1.0 + alpha;
     coeffs[4] = -2.0 * cos(w0);
-    coeffs[5] = 1.0 - alpha;
+    coeffs[5] =  1.0 - alpha;
+    // clang-format on
 }
 
 EQBQ_CDEF void eqbq_high_pass(double coeffs[6], double norm_freq, double q)
 {
     const double w0 = 2.0 * EQBQ_PI * norm_freq;
     const double alpha = sin(w0) / (2 * q);
-    coeffs[0] = (1.0 + cos(w0)) / 2.0;
+    // clang-format off
+    coeffs[0] =  (1.0 + cos(w0)) / 2.0;
     coeffs[1] = -(1.0 + cos(w0));
-    coeffs[2] = (1.0 + cos(w0)) / 2.0;
-    coeffs[3] = 1.0 + alpha;
+    coeffs[2] =  (1.0 + cos(w0)) / 2.0;
+    coeffs[3] =  1.0 + alpha;
     coeffs[4] = -2.0 * cos(w0);
-    coeffs[5] = 1.0 - alpha;
+    coeffs[5] =  1.0 - alpha;
+    // clang-format on
 }
 
 EQBQ_CDEF void eqbq_band_pass_skirt_gain(double coeffs[6], double norm_freq,
@@ -65,12 +69,14 @@ EQBQ_CDEF void eqbq_band_pass_skirt_gain(double coeffs[6], double norm_freq,
 {
     const double w0 = 2.0 * EQBQ_PI * norm_freq;
     const double alpha = sin(w0) / (2 * q);
+    // clang-format off
     coeffs[0] = sin(w0) / 2.0;
     coeffs[1] = 0.0;
     coeffs[2] = sin(w0) / 2.0;
-    coeffs[3] = 1.0 + alpha;
+    coeffs[3] =  1.0 + alpha;
     coeffs[4] = -2.0 * cos(w0);
-    coeffs[5] = 1.0 - alpha;
+    coeffs[5] =  1.0 - alpha;
+    // clang-format on
 }
 
 EQBQ_CDEF void eqbq_band_pass_peak_gain(double coeffs[6], double norm_freq,
@@ -78,36 +84,42 @@ EQBQ_CDEF void eqbq_band_pass_peak_gain(double coeffs[6], double norm_freq,
 {
     const double w0 = 2.0 * EQBQ_PI * norm_freq;
     const double alpha = sin(w0) / (2 * q);
-    coeffs[0] = alpha;
-    coeffs[1] = 0.0;
+    // clang-format off
+    coeffs[0] =  alpha;
+    coeffs[1] =  0.0;
     coeffs[2] = -alpha;
-    coeffs[3] = 1.0 + alpha;
+    coeffs[3] =  1.0 + alpha;
     coeffs[4] = -2.0 * cos(w0);
-    coeffs[5] = 1.0 - alpha;
+    coeffs[5] =  1.0 - alpha;
+    // clang-format on
 }
 
 EQBQ_CDEF void eqbq_notch(double coeffs[6], double norm_freq, double q)
 {
     const double w0 = 2.0 * EQBQ_PI * norm_freq;
     const double alpha = sin(w0) / (2 * q);
-    coeffs[0] = 1.0;
+    // clang-format off
+    coeffs[0] =  1.0;
     coeffs[1] = -2.0 * cos(w0);
-    coeffs[2] = 1.0;
-    coeffs[3] = 1.0 + alpha;
+    coeffs[2] =  1.0;
+    coeffs[3] =  1.0 + alpha;
     coeffs[4] = -2.0 * cos(w0);
-    coeffs[5] = 1.0 - alpha;
+    coeffs[5] =  1.0 - alpha;
+    // clang-format on
 }
 
 EQBQ_CDEF void eqbq_all_pass(double coeffs[6], double norm_freq, double q)
 {
     const double w0 = 2.0 * EQBQ_PI * norm_freq;
     const double alpha = sin(w0) / (2 * q);
-    coeffs[0] = 1.0 - alpha;
+    // clang-format off
+    coeffs[0] =  1.0 - alpha;
     coeffs[1] = -2.0 * cos(w0);
-    coeffs[2] = 1.0 + alpha;
-    coeffs[3] = 1.0 + alpha;
+    coeffs[2] =  1.0 + alpha;
+    coeffs[3] =  1.0 + alpha;
     coeffs[4] = -2.0 * cos(w0);
-    coeffs[5] = 1.0 - alpha;
+    coeffs[5] =  1.0 - alpha;
+    // clang-format on
 }
 
 EQBQ_CDEF void eqbq_peaking_eq(double coeffs[6], double norm_freq, double q,
@@ -116,12 +128,14 @@ EQBQ_CDEF void eqbq_peaking_eq(double coeffs[6], double norm_freq, double q,
     const double w0 = 2.0 * EQBQ_PI * norm_freq;
     const double alpha = sin(w0) / (2 * q);
     const double A = pow(10.0, gain_db / 40.0);
-    coeffs[0] = 1.0 + alpha * A;
+    // clang-format off
+    coeffs[0] =  1.0 + alpha * A;
     coeffs[1] = -2.0 * cos(w0);
-    coeffs[2] = 1.0 - alpha * A;
-    coeffs[3] = 1.0 + alpha / A;
+    coeffs[2] =  1.0 - alpha * A;
+    coeffs[3] =  1.0 + alpha / A;
     coeffs[4] = -2.0 * cos(w0);
-    coeffs[5] = 1.0 - alpha / A;
+    coeffs[5] =  1.0 - alpha / A;
+    // clang-format on
 }
 
 EQBQ_CDEF void eqbq_low_shelf(double coeffs[6], double norm_freq, double q,
@@ -130,12 +144,14 @@ EQBQ_CDEF void eqbq_low_shelf(double coeffs[6], double norm_freq, double q,
     const double w0 = 2.0 * EQBQ_PI * norm_freq;
     const double alpha = sin(w0) / (2 * q);
     const double A = pow(10.0, gain_db / 40.0);
-    coeffs[0] = A * ((A + 1.0) - (A - 1.0) * cos(w0) + 2.0 * sqrt(A) * alpha);
-    coeffs[1] = 2.0 * A * ((A - 1.0) - (A + 1.0) * cos(w0));
-    coeffs[2] = A * ((A + 1.0) - (A - 1.0) * cos(w0) - 2.0 * sqrt(A) * alpha);
-    coeffs[3] = (A + 1.0) + (A - 1.0) * cos(w0) + 2.0 * sqrt(A) * alpha;
-    coeffs[4] = -2.0 * ((A - 1.0) + (A + 1.0) * cos(w0));
-    coeffs[5] = (A + 1.0) + (A - 1.0) * cos(w0) - 2.0 * sqrt(A) * alpha;
+    // clang-format off
+    coeffs[0] =        A * ((A + 1.0) - (A - 1.0) * cos(w0) + 2.0 * sqrt(A) * alpha);
+    coeffs[1] =  2.0 * A * ((A - 1.0) - (A + 1.0) * cos(w0));
+    coeffs[2] =        A * ((A + 1.0) - (A - 1.0) * cos(w0) - 2.0 * sqrt(A) * alpha);
+    coeffs[3] =             (A + 1.0) + (A - 1.0) * cos(w0) + 2.0 * sqrt(A) * alpha;
+    coeffs[4] = -2.0 *     ((A - 1.0) + (A + 1.0) * cos(w0));
+    coeffs[5] =             (A + 1.0) + (A - 1.0) * cos(w0) - 2.0 * sqrt(A) * alpha;
+    // clang-format on
 }
 
 EQBQ_CDEF void eqbq_high_shelf(double coeffs[6], double norm_freq, double q,
@@ -144,12 +160,14 @@ EQBQ_CDEF void eqbq_high_shelf(double coeffs[6], double norm_freq, double q,
     const double w0 = 2.0 * EQBQ_PI * norm_freq;
     const double alpha = sin(w0) / (2 * q);
     const double A = pow(10.0, gain_db / 40.0);
-    coeffs[0] = A * ((A + 1.0) + (A - 1.0) * cos(w0) + 2.0 * sqrt(A) * alpha);
+    // clang-format off
+    coeffs[0] =        A * ((A + 1.0) + (A - 1.0) * cos(w0) + 2.0 * sqrt(A) * alpha);
     coeffs[1] = -2.0 * A * ((A - 1.0) + (A + 1.0) * cos(w0));
-    coeffs[2] = A * ((A + 1.0) + (A - 1.0) * cos(w0) - 2.0 * sqrt(A) * alpha);
-    coeffs[3] = (A + 1.0) - (A - 1.0) * cos(w0) + 2.0 * sqrt(A) * alpha;
-    coeffs[4] = 2.0 * ((A - 1.0) - (A + 1.0) * cos(w0));
-    coeffs[5] = (A + 1.0) - (A - 1.0) * cos(w0) - 2.0 * sqrt(A) * alpha;
+    coeffs[2] =        A * ((A + 1.0) + (A - 1.0) * cos(w0) - 2.0 * sqrt(A) * alpha);
+    coeffs[3] =             (A + 1.0) - (A - 1.0) * cos(w0) + 2.0 * sqrt(A) * alpha;
+    coeffs[4] =  2.0 *     ((A - 1.0) - (A + 1.0) * cos(w0));
+    coeffs[5] =             (A + 1.0) - (A - 1.0) * cos(w0) - 2.0 * sqrt(A) * alpha;
+    // clang-format on
 }
 
 #undef EQBQ_PI
